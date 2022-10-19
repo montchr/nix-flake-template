c0df10f46ac35411ce4c37b62f7268b505334639
Chris Montgomery <chris@cdom.io>
2022-10-19 17:05:48 -0400

    chore: add `reuse` command and generate licenses + headers

--- nix/devshells/profiles/common.nix
+++ nix/devshells/profiles/common.nix
@@ -1,5 +1,14 @@
+# SPDX-FileCopyrightText: 2022 Chris Montgomery
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 {pkgs, ...}: {
-  commands = [];
+  commands = [
+    {
+      category = "legal";
+      package = pkgs.reuse;
+    }
+  ];
   packages = [];
   env = [];
 }
