7bbc5d62f21c0be4df7ec2afa0fa1356ca9a90d0
Chris Montgomery <chris@cdom.io>
2022-10-19 15:44:14 -0400

    chore: cleanup

--- flake.nix
+++ flake.nix
@@ -3,25 +3,19 @@
 
   # Enable as needed for bootstrapping. Otherwise these entries cause warnings on every rebuild.
   # nixConfig.extra-experimental-features = "nix-command flakes";
-  # nixConfig.extra-substituters = "https://dotfield.cachix.org https://nix-community.cachix.org";
+  # nixConfig.extra-substituters = "https://nix-flake-template.cachix.org https://dotfield.cachix.org https://nix-community.cachix.org";
   # nixConfig.extra-trusted-public-keys = "dotfield.cachix.org-1:b5H/ucY/9PDARWG9uWA87ZKWUBU+hnfF30amwiXiaNk= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
 
   inputs = {
-    nixos-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
-    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-utils.url = "github:numtide/flake-utils";
-    nix-std.url = "github:chessai/nix-std";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     devshell.url = "github:numtide/devshell";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-flake.url = "github:srid/treefmt-flake";
     nixago.url = "github:nix-community/nixago";
-    sops-nix.url = "github:Mic92/sops-nix";
 
     flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     nixago.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs.follows = "nixos-unstable";
   };
 
   outputs = {
