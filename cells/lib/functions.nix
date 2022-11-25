e3287e4969a4fd58b0abf3ffc5f792d64f407f78
Chris Montgomery <chris@cdom.io>
2022-11-24 21:38:36 -0500

    feat(lib): add `tern` function for ternary expressions

--- cells/lib/functions.nix
+++ cells/lib/functions.nix
@@ -1,12 +1,24 @@
 # SPDX-FileCopyrightText: 2022 Chris Montgomery <chris@cdom.io>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-{
-  inputs,
-  cell,
-}: let
+{inputs}: let
   l = inputs.nixpkgs.lib // builtins;
 in {
+  /*
+  Functional sugar for a simple conditional ternary statement.
+
+  Type: Bool -> Any -> Any
+
+  Example:
+    let foo = "bar"; in
+    tern (foo == "bar") "fnord" "word"
+    => "fnord"
+  */
+  tern = p: y: n:
+    if p
+    then y
+    else n;
+
   /*
   Convert a list of strings to an attrset where the keys match the values.
 
