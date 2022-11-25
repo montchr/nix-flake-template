dc48134d0f18245b95a54dd085834e874842a1d0
Chris Montgomery <chris@cdom.io>
2022-11-24 21:44:13 -0500

    fix: restore file args broken by deadnix

--- cells/_automation/nixago.nix
+++ cells/_automation/nixago.nix
@@ -1,6 +1,9 @@
 # SPDX-FileCopyrightText: 2022 Chris Montgomery <chris@cdom.io>
 # SPDX-License-Identifier: GPL-3.0-or-later
-{inputs}: let
+{
+  inputs,
+  cell,
+}: let
   inherit (inputs) nixpkgs;
   l = inputs.nixpkgs.lib // builtins;
 in
